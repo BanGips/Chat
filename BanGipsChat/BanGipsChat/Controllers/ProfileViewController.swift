65ba2a54a135e5cdf1fa5305845892e4e9374bcd
Sergey Kast <sergeykast@Mac-mini-Sergey.local>
2021-01-25 00:06:00 +0300

    add profile VC

---
+++ BanGipsChat/BanGipsChat/Controllers/ProfileViewController.swift
@@ -0,0 +1,99 @@
+//
+//  ProfileViewController.swift
+//  BanGipsChat
+//
+//  Created by Sergey Kast on 24.01.21.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    let containerView = UIView()
+    let imageView = UIImageView(image: #imageLiteral(resourceName: "human1"), contentMode: .scaleAspectFill)
+    let nameLabel = UILabel(text: "PETER DAGGER", font: .systemFont(ofSize: 20, weight: .light))
+    let aboutMeLabel = UILabel(text: "fasfasfasfasfasfasfasffsdfsdfsdfsdfsdfsdfsdfsdfsdfafafaf", font: .systemFont(ofSize: 16, weight: .light))
+    let myTextField = UITextField()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .lightGray
+        customizeElements()
+        setupConstraints()
+        myTextField.borderStyle = .roundedRect
+    }
+    
+    private func customizeElements() {
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        aboutMeLabel.translatesAutoresizingMaskIntoConstraints = false
+        myTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        aboutMeLabel.numberOfLines = 0
+        containerView.backgroundColor = .mainWhite()
+        containerView.layer.cornerRadius = 30
+    }
+    
+}
+
+extension ProfileViewController {
+    
+    private func setupConstraints() {
+        view.addSubview(imageView)
+        view.addSubview(containerView)
+        containerView.addSubview(nameLabel)
+        containerView.addSubview(aboutMeLabel)
+        containerView.addSubview(myTextField)
+        
+        containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        containerView.heightAnchor.constraint(equalToConstant: 206).isActive = true
+        
+        imageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: containerView.topAnchor, constant: 30).isActive = true
+        
+        
+        nameLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 35).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 24).isActive = true
+        nameLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -24).isActive = true
+        
+        aboutMeLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
+        aboutMeLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 24).isActive = true
+        aboutMeLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -24).isActive = true
+        
+        myTextField.topAnchor.constraint(equalTo: aboutMeLabel.bottomAnchor, constant: 8).isActive = true
+        myTextField.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 24).isActive = true
+        myTextField.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -24).isActive = true
+        myTextField.heightAnchor.constraint(equalToConstant: 48).isActive = true
+    }
+}
+
+// MARK: - SwiftUI
+import SwiftUI
+
+struct ProfileControllerProvider: PreviewProvider {
+    
+    static var previews: some View {
+        Group {
+            ContainerView().edgesIgnoringSafeArea(.all)
+        }
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let viewController = ProfileViewController()
+        
+        func makeUIViewController(context: Context) -> some UIViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+        }
+    }
+}
