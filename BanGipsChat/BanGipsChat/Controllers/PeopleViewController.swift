43fcba7ec3243a027c29d966e2fa5efb1201574d
BanGips <bangips@MacBook-Pro-BanGips.local>
2021-01-24 14:19:16 +0300

    search

--- BanGipsChat/BanGipsChat/Controllers/PeopleViewController.swift
+++ BanGipsChat/BanGipsChat/Controllers/PeopleViewController.swift
@@ -32,7 +32,7 @@ class PeopleViewController: UIViewController {
         setupSearchBar()
         setupCollectionView()
         createDataSource()
-        reloadData()
+        reloadData(with: nil)
         
     }
     
@@ -47,10 +47,12 @@ class PeopleViewController: UIViewController {
         collectionView.register(forType: UserCell.self)
     }
     
-    private func reloadData() {
+    private func reloadData(with searchText: String?) {
+        let usersFilter = users.filter { $0.contains(filter: searchText) }
+            
         var snapshot = NSDiffableDataSourceSnapshot<Section, MUser>()
         snapshot.appendSections([.users])
-        snapshot.appendItems(users, toSection: .users)
+        snapshot.appendItems(usersFilter, toSection: .users)
         dataSource?.apply(snapshot, animatingDifferences: true)
     }
 
@@ -143,7 +145,7 @@ extension PeopleViewController {
 extension PeopleViewController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print(searchText)
+        reloadData(with: searchText)
     }
     
 }
